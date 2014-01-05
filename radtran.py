62ab0feab3137a3e9210d50684f174bbf61e0d55
Jacques Malaprade <ucfajrm@live.ucl.ac.uk>
2014-01-05 15:23:09 +0000

    Added the H function and Big psi kernel and plot functionality to test both.

--- radtran.py
+++ radtran.py
@@ -45,11 +45,11 @@ def gl(angle, arch='s', par=None):
       gl =  np.ones(np.shape(angle))*2./np.pi
     else:
       gl = 2./np.pi
-  elif arch=='k': # kuusk
+  elif arch=='k': # kuusk. had to multiply by factor below to work.
     if type(par)==tuple:
-      gl = lad.kuusk_lad(angle,par[0],par[1])
+      gl = lad.kuusk_lad(angle,par[0],par[1])/4.**2*np.pi**2
     else:
-      gl = lad.kuusk_lad(angle)
+      gl = lad.kuusk_lad(angle)/4.**2*np.pi**2
   elif arch=='b': # campbell
     if type(par)==tuple:
       gl = lad.elliptical_lad(angle,par[0],par[1])
@@ -154,6 +154,56 @@ def f(view, angle=0., sun=0., arch='s', refl=0.2, trans=0.1):
   '''
   return (refl + trans)/4./np.pi
 
+def H(view, angle):
+  '''The H function as described in Shultis 1988 (2.25) and Myneni
+  V.20 - V.22 and Knyazikhin 2004. 
+  Input: view - view or sun zenith angle, angle - leaf zenith angle.
+  Output: H function value.
+  '''
+  mu = np.cos(view)
+  mul = np.cos(angle)
+  with warnings.catch_warnings(): # the will only work in single thread app
+    warnings.simplefilter("ignore")
+    cotcot = 1./(np.tan(view)*np.tan(angle))
+  if isinstance(mul, np.ndarray):
+    h = np.zeros_like(mul)
+    for i, m in enumerate(mul):
+      if cotcot[i] > 1.:
+        h[i] = mu*m
+      elif cotcot[i] < -1.:
+        h[i] = 0.
+      else:
+        phit = np.arccos(cotcot[i])
+        h[i] = 1./np.pi * (mu*m*phit + np.sqrt(1.-mu**2.)*\
+            np.sqrt(1.-m**2.)*np.sin(phit))
+  else:
+    if cotcot > 1.:
+      h = mu*mul
+    elif cotcot < -1.:
+      h = 0.
+    else:
+      phit = np.arccos(cotcot)
+      h = 1./np.pi * (mu*mul*phit + np.sqrt(1.-mu**2.)*\
+          np.sqrt(1.-mul**2.)*np.sin(phit))
+  return h
+
+def Big_psi(view,sun,leaf,trans_refl):
+  '''The kernel which replaces the azimuth dependence
+  of the double integral based on the Myneni V.20.
+  Input: view - the view zenith angle in radians,
+    sun - the sun/illumination zenith angle,
+    leaf - the leaf zenith angel in radians,
+    trans_refl - 'r' reflectance or 't' transmittance.    
+  Output: The kernel.
+  '''
+  if trans_refl == 't':
+    B_psi = H(view,leaf)*H(sun,leaf) + H(-view,leaf)*H(-sun,leaf)
+  elif trans_refl == 'r':
+    B_psi = H(view,leaf)*H(-sun,leaf) + H(-view,leaf)*H(sun,leaf)
+  else:
+    raise Exception('IncorrectRTtype')
+  return B_psi
+
 def Gamma(view, angle=0., sun=0., arch='s', refl=0.2, trans=0.1):
   '''The Area Scattering Phase Function based on Myneni V.18
   and Shultis (17) isotropic scattering assumption. A more 
@@ -168,8 +218,8 @@ def Gamma(view, angle=0., sun=0., arch='s', refl=0.2, trans=0.1):
   '''
   B = view - sun
   gam = 4.*np.pi/(refl + trans)*f(view, refl=refl, trans=trans)\
-      /3./np.pi*(np.sin(B) - B*np.cos(B)) + trans/3.*np.cos(B)
-  
+      /3./np.pi*(np.sin(B) - B*np.cos(B)) + trans/3.*np.cos(B) # Myneni V.15
+   
   return gam 
 
 #def P(
@@ -231,9 +281,78 @@ def plotGamma():
   plt.legend(title='trans./albedo')
   plt.show()
 
-  
+def plotH():
+  '''A function that plots the H values for use in the 
+  Area Scattering Phase Function.
+  '''
+  x = np.linspace(0., np.pi, 500) # view or sun zenith angles
+  y = x.copy() # leaf normal zenith angles
+  xx, yy = np.meshgrid(x,y)
+  h = np.zeros_like(xx)
+  for i in range(len(x)):
+    for j in range(len(y)):
+      h[i,j] = H(xx[i,j], yy[i,j])
+  #pdb.set_trace()
+  plt.pcolormesh(xx, yy, h)
+  plt.axis([xx.max(),xx.min(),yy.max(),yy.min()])
+  plt.title('H Function Values')
+  plt.xlabel('Zenith angle view/sun (radians)')
+  plt.ylabel('Zenith angle leaf normal (radians)')
+  plt.colorbar()
+  #print np.where(h==np.inf)#np.diagonal(np.fliplr(h))[20:35]
+  plt.show()
+
+def plotBpsi():
+  '''A function that plots the Big psi values for a selection
+  of view angles.
+  Noticed that negative values for reflectance are correct and
+  positive values for transmittance also. If you follow each case
+  note that this does actually tell you alot about the influence
+  of angles on the reflectance and transmittance of the radiation.
+  '''
+  x = np.linspace(0., np.pi, 100) # sun zenith angles
+  y = x.copy() # leaf zenith angles
+  xx, yy = np.meshgrid(x,y)
+  view = np.linspace(0.,np.pi,5) # view zenith angles
+  trans_refl = ('r','t')
+  Bpsi = np.zeros_like(xx)
+  #pdb.set_trace()
+  fig, axarr = plt.subplots(2, len(view), sharex=True, sharey=True)
+  for row, rt in enumerate(trans_refl):
+    for col, v in enumerate(view):
+      if row != 0:
+        add = len(view)
+      else:
+        add = 0
+      #plt.subplot(2, len(view), col+1 + add)
+      for i in range(len(x)):
+        for j in range(len(y)):
+          Bpsi[i,j] = Big_psi(v, xx[i,j], yy[i,j], rt)
+      ax = axarr[row][col]
+      im = ax.pcolormesh(xx, yy, Bpsi, vmin=-0.4, vmax=1.)
+      #ax.colorbar()
+      if row == 0:
+        ax.set_title('Kernel view zenith\nangle @ %.1f degrees'\
+            % (v*180./np.pi))
+      #pdb.set_trace()
+      ax.axis([xx.max(),xx.min(),yy.max(),yy.min()])
+      if row == 1:
+        ax.set_xlabel('Zenith angle illum. (radians)')
+      if col == 0:
+        if row == 0:
+          ax.set_ylabel('Zenith angle leaf (radians)\n'+
+              'Reflectance or back to source')
+        else:
+          ax.set_ylabel('Zenith angle leaf (radians)\n'+
+              'Transmittance or away from source')
+  cbar_ax = fig.add_axes([0.9, 0.15, 0.022, 0.7])
+  fig.subplots_adjust(wspace=0., hspace=0.)
+  fig.colorbar(im, cax=cbar_ax)
+  plt.show()
 
 #pdb.set_trace()
 #plotgl()
 #plotG()
-plotGamma()
+#plotGamma()
+#plotH()
+plotBpsi()
